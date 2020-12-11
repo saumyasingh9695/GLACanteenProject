7f57f42f8d920e81b8ea742f13592a65501894aa
saumyasingh9695 <saumya.singh_cs18@gla.ac.in>
2020-12-12 00:19:28 +0530

    created and connected the main activity with the data base

--- app/src/main/java/com/example/glacanteenproject/MainActivity.kt
+++ app/src/main/java/com/example/glacanteenproject/MainActivity.kt
@@ -1,11 +1,118 @@
 package com.example.glacanteenproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var signup_button_LoginPage: TextView
+    private lateinit var forgetpassword: TextView
+    private lateinit var loginbutton : Button
+    private lateinit var Auth: FirebaseAuth
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var login_phone : Button
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signup_button_LoginPage = findViewById(R.id.signup)
+        loginbutton = findViewById(R.id.login)
+        username=findViewById(R.id.username)
+        password=findViewById(R.id.password)
+        progressBar = findViewById(R.id.progressBar2)
+        Auth=FirebaseAuth.getInstance()
+
+        signup_button_LoginPage.setOnClickListener {
+            startActivity(Intent(this,SignupPage::class.java))
+        }
+
+        loginbutton.setOnClickListener {
+            doLogin()
+            //startActivity( Intent(this,ItemListPageAfterLoginUser::class.java))
+
+        }
+    }
+
+    private fun doLogin(){
+
+        if (username.text.toString().isEmpty()) {
+            username.error = "please enter username"
+            username.requestFocus()
+            return
+        }
+        if (password.text.toString().isEmpty()) {
+            password.error = "please enter password"
+            password.requestFocus()
+            return
+        }
+        if (password.text.toString().length < 6) {
+            password.error = "please enter proper password"
+            password.requestFocus()
+            return
+        }
+        progressBar.visibility = View.VISIBLE
+        loginbutton.isEnabled = false
+        login_phone.isEnabled = false
+        signup_button_LoginPage.isEnabled = false
+        forgetpassword.isEnabled = false
+        Auth.signInWithEmailAndPassword(username.text.toString(),password.text.toString()).addOnCompleteListener{
+                task ->
+            if(task.isSuccessful){
+                val user: FirebaseUser?=Auth.currentUser
+                updateUI(user)
+            }
+
+            else{
+
+                updateUI(null)
+            }
+        }
+
+    }
+
+    fun updateUI(currentuser:FirebaseUser?){
+        if(currentuser!=null){
+            progressBar.visibility = View.GONE
+            loginbutton.isEnabled = true
+            login_phone.isEnabled = true
+            signup_button_LoginPage.isEnabled = true
+            forgetpassword.isEnabled = true
+            Toast.makeText(baseContext,"Login sucsessful", Toast.LENGTH_SHORT).show()
+            //val intent = Intent(this,ItemListPageAfterLoginUser::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else{
+            /*if(checker == 0){
+                checker = 1
+            }else {
+                Toast.makeText(baseContext, "Login Failed", Toast.LENGTH_SHORT).show()
+            }*/
+            Toast.makeText(baseContext, "Login Failed", Toast.LENGTH_SHORT).show()
+            progressBar.visibility = View.GONE
+            loginbutton.isEnabled = true
+            login_phone.isEnabled = true
+            signup_button_LoginPage.isEnabled = true
+            forgetpassword.isEnabled = true
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val user:FirebaseUser?=Auth.currentUser
+        //updateUI(user)
+        if(user!=null){
+            Toast.makeText(baseContext,"Login sucsessful", Toast.LENGTH_SHORT).show()
+            //startActivity(Intent(this,ItemListPageAfterLoginUser::class.java))
+            finish()
+        }
+
     }
 }
